964049480be97ace7bd8ffa46a97a85161856452
Jackson <slts0405@gmail.com>
2023-02-15 17:46:35 +0900

    로직 추가

--- algorithm/queue3.py
+++ algorithm/queue3.py
@@ -1,5 +1,6 @@
 # https://www.acmicpc.net/problem/3190
 # 골드4
+from collections import deque
 
 def changeDirection(direction, command):
     if command == 'L':
@@ -9,14 +10,29 @@ def changeDirection(direction, command):
     else:
         return direction
 
+# 맵 크기 n x n 입력받기
 n=int(input())
+# 사과 개수
 k=int(input())
-print(n, k)
+
+# 사과 위치 입력받고 맵에 사과위치 2로 표시
+field=[[0] * n for _ in range(n)]
+for _ in range(k):
+    ax, ay=map(int, input().split())
+    field[ax-1][ay-1]=2
+
+turnAfterTime={}
+# 특정 시간 후 방향전환 데이터 입력받고 객체형태로 저장
+l=int(input())
+for _ in range(l):
+    second, shift=input().split()
+    turnAfterTime[int(second)]=shift
+
 # 0: 위
 # 1: 오른쪽
 # 2: 아래
 # 3: 왼쪽
-d=0
+direction=0
 
 # 0: 위로 이동
 # 1: 오른쪽 이동
@@ -25,11 +41,19 @@ d=0
 dx=[0, 1, 0, -1]
 dy=[-1, 0, 1, 0]
 x, y=0, 0
-field=[[0] * 10 for _ in range(10)]
 
-# while 1:
-#     c=input()
-#     d=changeDirection(d, c)
-#     x+=dx[d]
-#     y+=dy[d]
-#     print(x, y)
+time=0
+
+# 벽에 몸이 부딪힌다
+# 자기 몸에 부딪힌다
+while 1:
+    if n <= x+=dx[d] < 0:
+        break
+    x+=dx[d]
+    y+=dy[d]
+    c=input()
+    directiond=changeDirection(direction, c)
+    print(x, y)
+    time+=time
+
+print(time)
