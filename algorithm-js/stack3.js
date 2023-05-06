6bf75157617353b4456d3fb08dffe4333f56e729
9z <slts0405@gmail.com>
2023-05-06 17:23:29 +0900

    fix(js): 조건 추가

--- algorithm-js/stack3.js
+++ algorithm-js/stack3.js
@@ -2,17 +2,22 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [N, K] = input[0].split(' ');
-const inputNumbers = input[1].split('');
+const inputNumbers = input[1].split('').map(inputNumber => Number(inputNumber));
 
 const stack = [];
 let count = K;
 
 inputNumbers.forEach((number) => {
-    while (count > 0 && number > stack[stack.length - 1]) {
+    while (count > 0 && stack.length > 0 && number > stack[stack.length - 1]) {
         stack.pop();
         count--;
     }
     stack.push(number);
 });
+
+while (count > 0) {
+    stack.pop();
+    count--;
+}
 
 console.log(stack.join(''));
